35755b14c09ba0021f507bb19c4c015bb60e298d
Trần Quang Châu <tranquangchau155@gmail.com>
2017-04-15 20:56:53 +0700

    Create index_read_class_style.php

---
+++ tests/index_read_class_style.php
@@ -0,0 +1,22 @@
+<h1>Read tag class css</h1>
+<?php
+
+
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+
+$file_read = "font-awesome.min.css";
+$oParser = new Sabberworm\CSS\Parser(file_get_contents($file_read));
+$oDoc = $oParser->parse();
+
+$after = $oDoc->aContents;
+
+//$can_xoa_tai = "#header";
+foreach ($after as $key => $value) {
+    //echo $key;
+    $kiem_t_tai = $value->aSelectors[0]->sSelector; //string hien tai
+	$kiem_t_tai=substr($kiem_t_tai,1); //remove .
+	
+	echo ($kiem_t_tai.'<br>');
+
+}
